a0a17478f342a6b2a4b4bf643757f076e892dadf
WellingtonSilva <wsilva@usp.br>
2016-11-25 16:43:51 -0200

    added some comments to easy understand

--- demo-pid.go
+++ demo-pid.go
@@ -7,11 +7,14 @@ import (
 	"syscall"
 )
 
-// go run demo.go run image commands arguments
+// go run demo-pid.go run image commands arguments
+// ou go build demo-pid.go && ./demo-pid run image commands arguments
+// like docker run [options] image [commands and arguments]
 func main() {
 
 	switch os.Args[1] {
 	case "run":
+		// debug message
 		defer exiting()
 		run()
 	case "child":
@@ -19,7 +22,6 @@ func main() {
 	default:
 		panic("Shit just happened...")
 	}
-
 }
 
 func exiting() {
@@ -27,29 +29,37 @@ func exiting() {
 }
 
 func run() {
+	// fork the process
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// creating new namespaces
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
-
 	doStuff(cmd.Run())
 }
 
 func child() {
 
+	// debug message
 	fmt.Printf("Rodando %v as PID %d usando imagem %v\n", os.Args[3:], os.Getpid(), os.Args[2])
 
+	//parsing the command
 	cmd := exec.Command(os.Args[3], os.Args[4:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// parsing the image to be used and pointing to its file system
 	rootfs := "/rootfs-" + os.Args[2]
 	doStuff(syscall.Chroot(rootfs))
 	doStuff(os.Chdir("/"))
+
+	//mounting /proc
 	doStuff(syscall.Mount("proc", "proc", "proc", 0, ""))
 	doStuff(cmd.Run())
 }
